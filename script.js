f13755c853bb325153a4ea9d2d7e0b3085e5d468
ANN KARUKU <annkaruku5832@gmail.com>
2022-07-13 17:07:18 +0300

    Page editings

--- script.js
+++ script.js
@@ -7,7 +7,36 @@ $(document).ready(function(){
         }else{
             $('.navbar2').removeClass("sticky");
         }
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
         
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    //typing animation
+    var typed= new Typed(".typing",{
+        strings:["Computer Scientist","Web developer","Android developer","IT support technician"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+     //typing animation
+     var typed2= new Typed(".typing2",{
+        strings:["Developer","Computer Scientist","IT support technician"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
     // toggle menu/navbar script
